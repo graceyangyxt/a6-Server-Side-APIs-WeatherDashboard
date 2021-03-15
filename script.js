230c7b5eb998a1404a2f98b277bb636b88bfedb5
Grace Yang <graceyxt.artdesign@gmail.com>
2021-03-14 23:48:36 -0400

    right column html/js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var searchForm = document.querySelector('#search-form');
 
 
 
-
+//getting the lat and long for the weather api call
 function getCoordinates(searchValue){
     var url = 'http://api.openweathermap.org/geo/1.0/direct?q=' + location + '&limit=1&appid=' + appid;
 
@@ -15,17 +15,19 @@ function getCoordinates(searchValue){
     .then(function(response){
         return response.json();
     })
+    console.log(response)
     .then(function(data){
         if(data.length){
             getWeather(data[0].name,data[0].lat,data[0].lon);
         }else{
-            todayEl.innerHTML = 'No results founds!';
+            todayEl.innerHTML = 'No results founds!'; 
             forecastEl.innerHTML = '';
         }
     })
 
 }
 
+//call the weather api and get back json data
 function getWeather(city, lat, lon){
     var url ='https://api.openweathermap.org/data/2.5/onecall?lat=' + lat + '&lon=' +lon+ '&exclude=hourly,minutely,alerts&units=imperial&appid=' +appid;
     
@@ -34,12 +36,42 @@ function getWeather(city, lat, lon){
         return response.json();
     })
     .then(function(data){
-        if()
+    if(data.length){
+      
+    }
     })
 } 
 
+
 function renderToday(city, today){
 
+    // accept location name and current object from getWeather as parameters
+
+    //clear out the today container -->  innerHTML=''
+   
+    //-- set textContent to location name(location parameter) and 
+    //the current date( moment or new Date().toLocalDateString())
+     var cardTitle= document.querySelector(".card-title");
+     cardTitle.textContent= //(location parameter) and the current date( moment or new Date().toLocalDateString())
+
+     //-->set text content to label and data property('Temperature:' + current.temp' + '°F')
+     //  <label id="lbltipAddedComment"></label>
+     document.getElementById('#windSpeed').innerHTML = 'Wind Speed:' + current.wind_speed;
+     document.getElementById('#temp').innerHTML = 'Temperature:' + current.temp + '°F';
+     document.getElementById('#humidity').innerHTML = 'Humidity:' + current.humidity ;
+     document.getElementById('#uv').innerHTML = 'UV:' + current.uvi;
+     var uvSpan = document.createElement("span");
+     uvSpan.setAttribute("class",btn);
+     uvSpan.textContent= current.uvi
+     //--> if, else statement to set the color to green if under 3, yellow if between 3 and 7, or red if above 7  (.btn-danger .btn-warning  .btn-success)
+
+
+    //  creat span
+    //  -->add any classes(.btn)
+    //  -->set textContent to uv index property
+    //  --> if, else statement to set the color to green if under 3, yellow if between 3 and 7, or red if above 7  (.btn-danger .btn-warning  .btn-success)
+
+    
 }
 
 function renderForecast(forecast){
